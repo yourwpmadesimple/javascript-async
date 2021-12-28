f25263e8f182fe124667d8d40dee1aaedb498576
Wayne Hatter <wayne@yourwpmadesimple.com>
2021-12-27 19:58:20 -0500

    README edit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # JavaScript Async
+
+```javascript
+// setTimeout is a built in JavaScript function that is asynchronous by default
+setTimeout(asyncFunct, 5000); 
+```
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const asyncFunct = () => {
+  console.log("Asynchronous result");
+};
+
 console.log("Hi!");
 
-setTimeout(function() {
-    console.log("Asynchronous result");
-}, 5000);
+// setTimeout is a built in JavaScript function that is asynchronous by default
+setTimeout(asyncFunct, 5000);
 
 console.log("Synchronous result.");
