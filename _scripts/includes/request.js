9aa0c12ab8ed6ee726621f8b56ca8a813fad338d
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-02 15:16:48 -0500

    end

--- _scripts/includes/request.js
+++ _scripts/includes/request.js
@@ -1,9 +1,9 @@
 export const get = (url, success, fail) => {
   let httpRequest = new XMLHttpRequest();
-  console.log(httpRequest);
   httpRequest.open("GET", url);
   httpRequest.onload = function () {
     if (httpRequest.status === 200) {
+      console.log(httpRequest.status);
       success(httpRequest.responseText);
     } else {
       fail(httpRequest.statusText);
