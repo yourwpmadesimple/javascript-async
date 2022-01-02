22b7cbce589126a6abe71e72fba857f8a6a1b4ba
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-02 17:37:05 -0500

    Examp 3 initial commit

--- _scripts/ajax.js
+++ _scripts/ajax.js
@@ -14,36 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "mariposa,us",
   ];
 
-  // const url =
-  //   "https://api.openweathermap.org/data/2.5/weather?q=charlotte&APPID=" +
-  //   apiKey;
   const urls = locations.map(function (location) {
     return `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
   });
-
-  //get(url, successHandler, failHandler);
-  //get(url)
-  Promise.all([get(urls[0]), get(urls[1]), get(urls[2]), get(urls[3])])
-    // .then(function (response) {
-    //   successHandler(response);
-    // })
-    .then(function (responses) {
-      return responses.map(function (response) {
-        return successHandler(response);
-      });
-    })
-    .then(function (literals) {
-      weatherDiv.innerHTML = `
-      <h1>Weather</h1>
-      ${literals.join("")}
-      `;
-    })
-    .catch(function (status) {
-      failHandler(status);
-      const weatherDiv = document.querySelector("#weather");
-      weatherDiv.classList.remove("hidden");
-    })
-    .finally(function () {
-      weatherDiv.classList.remove("hidden");
-    });
 });
