f9b227fb3fb70b6f51d76623e7815324214dd933
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-02 18:07:24 -0500

    looping through array of cities in async method

--- _scripts/ajax.js
+++ _scripts/ajax.js
@@ -3,8 +3,8 @@ import { successHandler } from "./includes/successHandler.js";
 import { failHandler } from "./includes/failHandler.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  //const apiKey = "082ff726cddf285ae3b74b8f9a32abda"; // ADD YOUR API KEY BETWEEN THE QUOTES
-  const apiKey = "";
+  const apiKey = "082ff726cddf285ae3b74b8f9a32abda"; // ADD YOUR API KEY BETWEEN THE QUOTES
+  //const apiKey = "";
   const weatherDiv = document.querySelector("#weather");
 
   const locations = [
@@ -21,15 +21,15 @@ document.addEventListener("DOMContentLoaded", () => {
   (async () => {
     try {
       let responses = [];
-      responses.push(await get(urls[0]));
-      responses.push(await get(urls[1]));
-      responses.push(await get(urls[2]));
-      responses.push(await get(urls[3]));
+      for (let i = 0; i < urls.length; i++) {
+        responses.push(await get(urls[i]));
+      }
+
       let literals = responses.map((response) => {
         return successHandler(response);
       });
       weatherDiv.innerHTML = `
-      <h1>Weather<./h1>
+      <h1>Weather</h1>
       ${literals.join("")}
       `;
       weatherDiv.classList.remove("hidden");
