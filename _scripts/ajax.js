32a37756e6febbd261ef7a50f22e632640dab7a3
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-02 17:54:54 -0500

    added try and catch blocks with error handling

--- _scripts/ajax.js
+++ _scripts/ajax.js
@@ -3,8 +3,8 @@ import { successHandler } from "./includes/successHandler.js";
 import { failHandler } from "./includes/failHandler.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const apiKey = "082ff726cddf285ae3b74b8f9a32abda"; // ADD YOUR API KEY BETWEEN THE QUOTES
-  //const apiKey = "";
+  //const apiKey = "082ff726cddf285ae3b74b8f9a32abda"; // ADD YOUR API KEY BETWEEN THE QUOTES
+  const apiKey = "";
   const weatherDiv = document.querySelector("#weather");
 
   const locations = [
